daed1c8891eab544bc7c983cd1e8b87de96adbfd
Divyanshu <divyanshuvishwakarma222@gmail.com>
2024-04-14 00:21:11 +0530

    ✅ Done with nevbar and start Hero section

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,12 @@
+
+
+export default function HeroSection() {
+  return (
+    <div className="h-auto md:h-[40rem] w-full rounded-md flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-8">
+        <div className=" p-4 relative z-10 w-full text-center">
+            <h1>Master the art of music</h1>
+            <p>Dive into our comprehensive music courses and transform your musical journey today. Whether you're a beginner or looking to refine your skills, join us to unlock your true potential.</p>
+        </div>
+    </div>
+  )
+}
